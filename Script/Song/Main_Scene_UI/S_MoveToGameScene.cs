29926d5cfc84a8be6f47b8450ad70c2a37ccffdc
pspsp456 <pspsp456@gmail.com>
2016-07-16 11:21:27 +0900

    add script

---
+++ Script/Song/Main_Scene_UI/S_MoveToGameScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_MoveToGameScene : MonoBehaviour
+{
+	void OnClick()
+	{
+		Application.LoadLevel ("S_Game_Main_UI");
+	}
+
+}
---
+++ Script/Song/Main_Scene_UI/S_SkillWindow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_SkillWindow : MonoBehaviour {
+	public GameObject viewObject;
+
+	private bool clicked = false;
+	public void OnClick()
+	{
+		if (!clicked) {
+			clicked = true;
+			Debug.Log ("눌림");
+		}
+		else {
+			clicked = false;
+		}
+
+		viewObject.SetActive (clicked);
+	}
+}
