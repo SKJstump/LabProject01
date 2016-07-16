2cac11dccbb9c642f4e9c33cfc76628ff8967b7c
pspsp456 <pspsp456@gmail.com>
2016-07-16 11:06:41 +0900

    S_Game_Main_UI ver0.03
    drag&drop

---
+++ Script/Song/Drag&drop/S_ClickSkill.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_ClickSkill : MonoBehaviour {
+	// Skill 그림을 위한 텍스쳐 이다.
+	public UITexture[] skillTexture;
+	public UISprite[] skillSprite;
+	// 클릭이 됐는지 확인하는 변수
+	public bool clicked;
+
+	// 그림을 바꾸기 위한 변수 선언 이다.
+	private UISprite sprite1;
+	private GameObject SkillObject;
+
+	void Start () {
+		sprite1 = this.gameObject.GetComponent<UISprite>();
+		SkillObject = this.gameObject;
+		clicked = true;
+	}
+
+	void Update()
+	{
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//SkillObject =  other.gameObject;
+
+	}
+
+	public void OnClick()
+	{
+		//Debug.Log ("충돌 됨");
+	
+
+		//sprite1 = skillSprite[3];
+		//Debug.Log (sprite1.spriteName);
+		if (!clicked) {
+			//sprite1.spriteName = skillSprite [0].spriteName;
+			clicked = true;
+		} else {
+			//sprite1.spriteName = skillSprite [1].spriteName;
+			clicked = false;
+			Debug.Log ("충돌 됨");
+		}
+		//SkillObject.SetActive (clicked);
+		sprite1.enabled = clicked;
+		//Debug.Log (sprite1.spriteName);
+
+	}
+
+}
---
+++ Script/Song/Drag&drop/SkillSpriteDrop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillSpriteDrop : MonoBehaviour {
+	// 스크립트 변수 참조를 우ㅣ한 선언.
+	private S_MouseCursorSprite skillObjectScript;
+	private bool Clicked = false;
+
+	// 어떤 스킬 슬롯을 사용할 것인가.
+	public GameObject mouseObject;
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	void OnClick()
+	{
+		if (skillObjectScript.GetComponent<UISprite> ().enabled) {
+			
+		}
+	}
+}
